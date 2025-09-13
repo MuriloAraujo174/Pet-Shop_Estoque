e7c2da8bb628197d1689b7b8f6fa8208ed3b9987
Antonio Murilo <antoniomurilog@gmail.com>
2025-09-13 19:35:04 -0300

    Chamandos os dados para listagem
    Esse metodo faz trazes os dados do banco para utilizar nos relatorios

--- src/dao/RelatorioDAO.java
+++ src/dao/RelatorioDAO.java
@@ -1,5 +1,44 @@
 package dao;
 
+import beans.produtos;
+import conexao.Conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class RelatorioDAO {
+    private Conexao conex; // Class conexao vai ser usada posteriomente
+    private Connection conn; // class de conexao 
+    // codigo sql de Pesquisar de dados
+    String sql = "select * from produto";
+    
+    public RelatorioDAO() {
+        this.conex = new Conexao();
+        this.conn = this.conex.getConnection();
+    }
+    
+    public void listaProdutosRelatorio() {
+    
+        try {
+            PreparedStatement st = this.conn.prepareStatement(sql);
+            
+            ResultSet rs = st.executeQuery();
+            
+            while(rs.next()) {
+                
+                System.out.println(rs.getInt("nome") + " - " + rs.getString("quant_ent") + " - " + rs.getString("preco"));
+                
+            }
+            
+        } catch (Exception e) {
+        }
+    
+    }
     
 }
